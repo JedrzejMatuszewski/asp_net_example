aea00f4a9fdcea741fdcd0b33b5c8122ec68709b
Jędrzej Matuszewski <jedrzej@matuszewski.co>
2020-12-22 22:57:35 +0100

    Project created

---
+++ MvcExample/Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcExample.Models;
+
+namespace MvcExample.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
